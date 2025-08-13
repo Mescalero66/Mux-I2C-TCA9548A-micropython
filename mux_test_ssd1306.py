128f9d00d3243db9eaf2decfc361704bdf23109b
Mescalero66 <mclauchlanjd@gmail.com>
2025-08-13 22:18:49 +1000

    done

---
+++ mux_test_ssd1306.py
@@ -0,0 +1,31 @@
+# import time, sys, framebuf, Pin, I2C
+from mux import I2CMultiplex
+from ssd1306 import SSD1306_I2C
+
+I2CMultiAddr = 0x70
+
+pix_res_x  = 128 # SSD1306 horizontal resolution
+pix_res_y = 64   # SSD1306 vertical resolution
+
+OLEDAddr = 0x3C
+OLEDCount = 4
+OLEDs = []   
+
+# Create multiplexer object
+mux = I2CMultiplex(I2CMultiAddr, I2Cbus=1, scl_pin=15, sda_pin=14)
+# Use mux.i2c as the shared I2C bus for OLEDs
+
+print("mux.i2c =", mux.i2c)
+print("type =", type(mux.i2c))
+
+# define OLEDs
+for i in range(OLEDCount):
+    mux.select_port(i)
+    OLEDs.append(SSD1306_I2C(pix_res_x, pix_res_y, mux.i2c))
+
+for i in range(OLEDCount):
+    mux.select_port(i)
+    OLEDs[i].fill(0)
+    text = f"OLED # {i}"
+    OLEDs[i].banner_text(text)
+    OLEDs[i].show()
